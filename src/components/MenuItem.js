87b3b10211f9ee816811e915602e0e614983a48e
Mübarek ALBAYATI <mubapc1@gmail.com>
2024-01-25 15:59:21 +0300

    React burger website

---
+++ src/components/MenuItem.js
@@ -0,0 +1,16 @@
+function MenuItem({ image, name, content, price }) {
+  return (
+    <div className="menuItem">
+      <div style={{ backgroundImage: `url(${image})` }}></div>
+      <h1 style={{ marginBottom:'10px'}}>{name}</h1>
+      <h6 style={{ marginBottom:'10px'}}>{content}</h6>
+      <p>
+        <i style={{ color:'red',fontWeight:'bold'}}>
+        {price} TL
+        </i>
+      </p>
+    </div>
+  );
+}
+
+export default MenuItem;
